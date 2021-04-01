24a4d1a62dbab693b1403579ed8bfdd3fa3a554a
sergedemanou <demanou05@yahoo.fr>
2021-04-01 09:28:22 +0100

    second commit

--- systeme_de_gestion_de_stock/projet/settings.py
+++ systeme_de_gestion_de_stock/projet/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'projet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'CRM-FORMASYS',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 
 }
